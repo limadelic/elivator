f66d4b3366e02b47d8a6bdb741e39f0d67a963cd
msuarz <maykel.suarez@ukg.com>
2025-10-02 20:50:26 -0400

    lobby done

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,3 +1,7 @@
 export default function Car({ position }) {
-  return <div className="car" style={{ bottom: position }} />
+  return (
+    <div className="car" style={{ bottom: position }}>
+      <div className="car-door-line" />
+    </div>
+  )
 }
